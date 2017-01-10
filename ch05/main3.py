7e185a8f3032e8ccebadda93888832e66b4c6db3
sky0621 <sky14work@gmail.com>
2017-01-11 02:00:09 +0900

    ch05, 06

---
+++ ch05/main3.py
@@ -0,0 +1,12 @@
+# set
+basket = {'python', 'go', 'java'}
+print(basket)
+
+a = set('abracatabra')
+print(a)
+
+# dict
+d = {'Taro': 'B', 'Jiro': 'A', 'Hanako': 'O'}
+for k, v in d.items():
+    print(k, v)
+    
---
+++ ch06/main.py
@@ -0,0 +1,9 @@
+import fibo
+
+fibo.fib(50)
+print("=============================")
+print(fibo.fib2(50))
+print("=============================")
+
+fb = fibo.fib
+fb(100)
