95cd1f5a4477294f7b6a91a17b4e7f59fbdb0e40
sky0621 <sky14work@gmail.com>
2018-07-09 23:33:19 +0900

    sample

---
+++ pickup/README.md
@@ -0,0 +1,11 @@
+# samples
+
+## setup
+<pre>
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ python -V
+Python 3.6.3
+$ pip -V
+pip 9.0.1
+</pre>
--- pickup/sample_for.py
+++ pickup/sample_for.py
@@ -1,4 +1,8 @@
-words = ['dog', 'cat', 'mouse']
+print("# # # # # # # # # # # # # # # # # # # #")
+print("listのループ")
+print("# # # # # # # # # # # # # # # # # # # #")
+
+words = ['dog', 'cat', 'mouse', 'rabbit', 'horse']
 for w in words:
     print(w, len(w))
 
@@ -6,3 +10,41 @@ for w2 in words[:]:
     words.insert(0, w2)
 
 print(words)
+
+print(" ")
+print("# # # # # # # # # # # # # # # # # # # #")
+print("range")
+print("# # # # # # # # # # # # # # # # # # # #")
+
+for i in range(5):
+    print(i)
+
+print(" ")
+
+for j in range(5, 10):
+    print(j)
+
+print(" ")
+
+for k in range(0, 10, 3):
+    print(k)
+
+print(" ")
+print("# # # # # # # # # # # # # # # # # # # #")
+print("iterable")
+print("# # # # # # # # # # # # # # # # # # # #")
+
+print(list(range(5)))
+
+print(" ")
+print("# # # # # # # # # # # # # # # # # # # #")
+print("for-else")
+print("# # # # # # # # # # # # # # # # # # # #")
+
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        print(n, 'is a prime number')
